0568166aacca030d57f49ecddd160185605a7ab6
hexaalif <hexaalif2020@gmail.com>
2023-06-09 12:36:16 +0600

    files

--- Vector_init_constructor.cpp
+++ Vector_init_constructor.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
-    vector<int> v;
+    // vector<int> v type 1
+    // vector<int> v(5); type 2
+    vector<int> v(5, 10);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 
     cout << v.size();
 
