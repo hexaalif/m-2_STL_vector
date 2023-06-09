64e174601797ac57dc31f4fc019fa563ac878bce
hexaalif <hexaalif2020@gmail.com>
2023-06-09 12:40:27 +0600

    files

--- Vector_init_constructor.cpp
+++ Vector_init_constructor.cpp
@@ -6,8 +6,15 @@ int main()
 {
     // vector<int> v type 1
     // vector<int> v(5); type 2
-    vector<int> v(5, 10);
+    // vector<int> v(5, 10); type 3
+    // vector<int> v2(5, 100);
+    // vector<int> v(v2); type 4
 
+    // type 5
+    // int a[6] = {1, 2, 3, 4, 5, 6};
+    // vector<int> v(a, a + 6);
+
+    vector<int> v = {1, 3, 4, 5, 6};
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
