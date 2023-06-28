c8fed01e9e62638d5f7ced736b62753043ae4a3e
hexaalif <hexaalif2020@gmail.com>
2023-06-28 12:02:41 +0600

    codeforce

--- J_Count_Letters.cpp
+++ J_Count_Letters.cpp
@@ -4,8 +4,36 @@ using namespace std;
 
 int main()
 {
-    string s;
-    cin >> s;
-    cout << s;
+    char c;
+    // cin >> s;
+    // int cnt[26] = {0};
+    // while ((cin >> c))
+    // {
+    //     int val = c - 'a';
+    //     cnt[val]++;
+    // }
+    // for (int i = 0; i < 26; i++)
+    // {
+    //     if (cnt[i] != 0)
+    //     {
+    //         // int val = c;
+    //         char ch = i + 97;
+    //         cout << ch << " : " << cnt[i] << endl;
+    //     }
+    // }
+    vector<int> cnt(26, 0);
+    while (cin >> c)
+    {
+        int val = c - 'a';
+        cnt[val]++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (cnt[i] != 0)
+        {
+            char ch = i + 97;
+            cout << ch << " : " << cnt[i] << endl;
+        }
+    }
     return 0;
 }
